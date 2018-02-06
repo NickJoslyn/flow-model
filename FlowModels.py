95436188b88f723e714b3a55ec3923f724a85dfa
Nick Joslyn <nick.joslyn@my.simpson.edu>
2018-02-06 10:03:17 -0600

    Create FlowModels.py
    Setting up FlowModels.py file. Commented thoughts.

---
+++ FlowModels.py
@@ -0,0 +1,11 @@
+## Python file for solving Flow Based Models
+## Function page -- call the defined function from another file
+
+# Import list of OD pairs
+# Run function on OD pairs according to Flow Model
+# Return optimal locations
+
+## Could have the optimization function an input parameter
+
+## Could be the case we have multiple functions here that we are able to call
+## FCLM, FRLM, SFCLM, . . .
