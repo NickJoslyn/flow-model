cfcce86e226b697be1aa0b43f181859f661139f7
Nick Joslyn <nick.joslyn@my.simpson.edu>
2018-02-06 10:13:53 -0600

    Create arcScripts.py

---
+++ arcScripts.py
@@ -0,0 +1,6 @@
+## ArcGIS will be utilized for spatial visualizations and data gathering
+## ArcGIS has a Python window for interfacing with shapefiles
+## It seems to be interactive only, so this file will be home to important commands
+
+# It is possible that ArcGIS will prove more valuable than coding LA models
+# If that occurs, this file will prove even more beneficial
